c3c110da0af7082ed2c61ead06a1f68a0ec144c5
zhyuelin <zhyuelin@163.com>
2019-03-03 09:14:57 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,94 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="inputInfo">
+      
+      <div class="formbox">
+        <el-form
+          :model="ruleForm2"
+          status-icon
+          :rules="rules2"
+          ref="ruleForm2"
+          label-width="100px"
+          class="demo-ruleForm"
+          align-center
+        >
+          <el-form-item>
+            <h5>登录</h5>
+          </el-form-item>
+          <el-form-item label="账号" prop="username">
+            <el-input v-model.number="ruleForm2.username"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="pass">
+            <el-input type="password" v-model="ruleForm2.pass" autocomplete="off"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+            <el-button @click="resetForm('ruleForm2')">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    var checkName = (rule, value, callback) => {
+      if (!value) {
+        return callback(new Error("用户名不能为空"));
+      } 
+      callback();
+    };
+    var validatePass = (rule, value, callback) => {
+      if (value === "") {
+        callback(new Error("请输入密码"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      ruleForm2: {
+        username: "",
+        pass: "",
+      },
+      rules2: {
+        pass: [{ validator: validatePass, trigger: "blur" }],
+        username: [{ validator: checkName, trigger: "blur" }]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("提交成功!");
+        } else {
+          console.log("提交错误!");
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.inputInfo {
+  margin: 0 auto;
+  width: 450px;
+}
+
+.inputInfo h5 {
+  margin-left: -20px;
+  font-size: 16px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-weight: normal;
+  color: #409eff;
 }
 </style>
+ 
